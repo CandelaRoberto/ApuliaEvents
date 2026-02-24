67b1f4727dc2768cad47f5031ac0feeae71f18bc
CandelaRoberto <roberto_candela@hotmail.it>
2026-02-24 10:14:23 +0100

    Delete Frontend/src directory

--- Frontend/src/Auth/Reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "../Slice/authSlice.js"; 
-
-const rootReducer = combineReducers({
-  auth: authReducer,
-});
-
-export default rootReducer;
