28953c570f3db7d61354f4807eeabee552e40169
CandelaRoberto <roberto_candela@hotmail.it>
2026-02-24 10:12:50 +0100

    Delete Backend/Middleware directory

--- Backend/Middleware/VerificaRuolo.js
+++
@@ -1,9 +0,0 @@
-export const VerificaRuolo = (req, res, next) => {
-  const user = req.user; 
-
-  if (!user || user.ruolo !== "Organizzatore") {
-    return res.status(403).json({ message: "Accesso negato: solo organizzatori" });
-  }
-
-  next();
-};
--- Backend/Middleware/multer.js
+++
@@ -1,7 +0,0 @@
-import multer from "multer";
-const storage = multer.memoryStorage();
-const upload = multer({ storage, limits: { fileSize: 3 * 1024 * 1024 } });
-
-export default upload;
-
-//multer salva i file in memoria temporaneamente
