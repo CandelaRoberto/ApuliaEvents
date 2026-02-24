e2cefb1044ed0dc5ec7ebf833bdd3da43a48a2d4
CandelaRoberto <roberto_candela@hotmail.it>
2026-02-24 10:13:10 +0100

    Delete Backend/Routes directory

--- Backend/Routes/Notifiche_route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import protectRoute from "../Middleware/ProtectRoute.js";
-import { creaNotifica,segnaComeVista} from "../Controller/Notifiche_controller.js";
-
-const router=express.Router();
-router.get('/notifiche', protectRoute, creaNotifica);
-router.put("/segnaComeVista", protectRoute, segnaComeVista);
-
-export default router;
