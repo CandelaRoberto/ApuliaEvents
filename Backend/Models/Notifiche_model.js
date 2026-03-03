581763a75b455394643c50792b2bdbec3f14a8b8
CandelaRoberto <roberto_candela@hotmail.it>
2026-03-03 17:10:03 +0100

    Update Notifiche_model.js

--- Backend/Models/Notifiche_model.js
+++ Backend/Models/Notifiche_model.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const NotificaSchema = new mongoose.Schema({
-  user: { type: Schema.Types.ObjectId, ref:"User", required:true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref:"User", required:true },
   type: { type:String, enum:["message","event"], required:true },
-  fromUser: { type: Schema.Types.ObjectId, ref:"User" },
-  event: { type: Schema.Types.ObjectId, ref:"Post" },
+  fromUser: { type: mongoose.Schema.Types.ObjectId, ref:"User" },
+  event: { type: mongoose.Schema.Types.ObjectId, ref:"Post" },
   count: { type:Number, default:1 },
   seen: { type:Boolean, default:false },
 },
@@ -12,5 +12,6 @@ const NotificaSchema = new mongoose.Schema({
 );
 
 export default mongoose.model("Notifiche", NotificaSchema);
+
 
 
